eb71d0fae713b8873c62da9d27feaf2c5d515147
John Glenn Andrade <johnglenn.andrade@gmail.com>
2023-05-16 04:32:29 +0800

    [NavigationalBar] Added styles for Navigation

--- src/global_components/AppWrapper/index.tsx
+++ src/global_components/AppWrapper/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Box } from "@mui/material"
 import { webStyle } from "./styles/AppWrapper.styles"
+import "./styles/AppWrapper.scss";
 
 interface ReactWrapper {
     children: React.ReactNode;
 }
 export const AppWrapper = ({ children }: ReactWrapper) => {
     return (
-        <Box sx={webStyle.AppStyle}>{children}</Box>
+        <Box sx={webStyle.AppStyle} className="app-wrapper">{children}</Box>
     )
 }
--- src/global_components/NavigationalBar/index.tsx
+++ src/global_components/NavigationalBar/index.tsx
@@ -12,6 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import "./styles/NavigationalBar.scss";
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -36,7 +37,7 @@ export const NavigationalBar: React.FC = () => {
     };
 
     return (
-        <AppBar position="static">
+        <AppBar position="static" className="navigational-bar">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
